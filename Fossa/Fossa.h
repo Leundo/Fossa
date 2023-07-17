642d386a75df3d8e96e753308ee5a5c8d94b7565
Leundo <daysundoing@outlook.com>
2023-07-17 12:01:36 +0800

    Initial Commit

---
+++ Fossa/Fossa.h
@@ -0,0 +1,18 @@
+//
+//  Fossa.h
+//  Fossa
+//
+//  Created by Undo Hatsune on 2023/07/17.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Fossa.
+FOUNDATION_EXPORT double FossaVersionNumber;
+
+//! Project version string for Fossa.
+FOUNDATION_EXPORT const unsigned char FossaVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Fossa/PublicHeader.h>
+
+
